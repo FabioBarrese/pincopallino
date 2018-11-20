a71f65526cad0199743a9c572ba37f2feb37a2ac
fabiobarrese <fabiobarrese@192.168.1.4>
2018-11-20 22:31:28 +0100

    progetto maven con test junit generici su una matrice

---
+++ MavenSecondTestSw/src/main/java/ingegneriaSW/esonero/MavenSecondTestSw/App.java
@@ -0,0 +1,36 @@
+package ingegneriaSW.esonero.MavenSecondTestSw;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+	}
+
+	public int sommaMatrice(int[][] m) {
+		int somma = 0;
+		for (int i = 0; i < m.length; i++)
+			for (int j = 0; j < m[0].length; j++)
+				somma += m[i][j];
+		if(somma<0)
+			throw new IllegalArgumentException("la somma non può essere negativa");
+		return somma;
+	}
+
+	public void ordinamento(int[][] m) {
+		for (int h = 0; h < m.length; h++) {
+			for (int i = 0; i < m[0].length - 1; i++) {
+				for (int j = i + 1; j < m[0].length; j++) {
+					if (m[h][i] > m[h][j]) {
+						int t = m[h][i];
+						m[h][i] = m[h][j];
+						m[h][j] = t;
+					}
+				}
+			}
+		}
+
+	}
+}
---
+++ MavenSecondTestSw/src/test/java/ingegneriaSW/esonero/MavenSecondTestSw/MyPerformanceTests.java
@@ -0,0 +1,24 @@
+package ingegneriaSW.esonero.MavenSecondTestSw;
+
+import java.util.Scanner;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyPerformanceTests {
+	private static App app = new App();
+	private static int[][] m = new int[10][10];
+	private static Scanner in = new Scanner(System.in);
+	
+	@BeforeClass
+	public static void init() {
+		for (int i = 0; i < m.length; i++)
+			for (int j = 0; j < m[0].length; j++)
+				m[i][j]=in.nextInt();
+	}
+	
+	@Test(timeout=1000)
+	public void OrderspeedTest() {
+		app.ordinamento(m);
+	}
+}
